dc182ae02278baafbd1e6a70f5d32b54d6f5c5c5
Ilya Pakhalko <ipahalko@gmail.com>
2021-06-07 12:41:37 +0300

    added chronological splitting

--- SAS_imbalanced/data/split.py
+++ SAS_imbalanced/data/split.py
@@ -1,7 +1,16 @@
+from sklearn.model_selection import train_test_split
+
 def split(df, strat='chrono', out_dir=None):
     '''
       accepts a dataframe with dataset, returns train, dev and test dataframe folds
       strat: how to split the data into folds; either 'chrono' or 'stratify'
       out_dir: if specified, write csvs to this directory
     '''
-    raise NotImplementedError
+    assert strat in ['chrono']
+
+    if strat == 'chrono':
+
+        df_train, df_test = train_test_split(df, test_size=0.2, shuffle=False)
+        df_dev, df_test = train_test_split(X_test, test_size=0.5, shuffle=False)
+
+        return df_train, df_dev, df_test
